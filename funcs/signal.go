5e8f335474bde73ec10e86a1aa450e6b85c965d1
BigKuCha <nevermore1989@vip.qq.com>
2016-12-29 14:01:08 +0800

    多通道接收相同信号

--- funcs/signal.go
+++ funcs/signal.go
@@ -6,9 +6,63 @@ import (
 	"fmt"
 	"os/signal"
 	"time"
+	"sync"
 )
 
 func TestSignal() {
+	//testNotify()
+	testNotify2()
+}
+
+//多个通道接收同一个信号
+func testNotify2() {
+	receiverOne := make(chan os.Signal, 1)
+	receiverTwo := make(chan os.Signal, 2)
+
+	sigs1 := []os.Signal{syscall.SIGINT}
+	sigs2 := []os.Signal{syscall.SIGINT, syscall.SIGQUIT}
+	signal.Notify(receiverOne, sigs1...)
+	signal.Notify(receiverTwo, sigs2...)
+	fmt.Println("receiverOne 只处理中断信号")
+	fmt.Println("receiverTwo 处理中断信号和退出信号")
+	go func() {
+		time.Sleep(10e9)
+		fmt.Println("程序运行了10s, receiverOne 不再处理信号")
+		signal.Stop(receiverOne)
+		time.Sleep(5e9)
+		fmt.Println("程序运行了15s, 不再处理任何信号") //按ctrl c 会结束运行
+		signal.Stop(receiverTwo)
+	}()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		for sig := range receiverOne {
+			if sig == syscall.SIGINT {
+				fmt.Println("receiverOne 接到了中断信号，程序继续处理")
+				continue
+			}
+			fmt.Println("receiverOne 接到了其他信号", sig)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for sig := range receiverTwo {
+			switch sig {
+			case syscall.SIGINT:
+				fmt.Println("receiverTwo 接到了中断信号，程序继续运行")
+			case syscall.SIGQUIT:
+				fmt.Println("receiverTwo 接到了退出信号，程序继续运行")
+			default:
+				fmt.Println("receiverTwo 接到了其他信号", sig)
+			}
+
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
+func testNotify() {
 	receiver := make(chan os.Signal, 1)
 	sigs := []os.Signal{syscall.SIGINT, syscall.SIGQUIT}
 	fmt.Printf("给接受者设置的信号为%s...[reciver]\n", sigs)
