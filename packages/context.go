1285393cea0c5ece9ca336853f786b8159a70db3
BigKuCha <nevermore1989@vip.qq.com>
2019-05-27 22:40:28 +0800

    context

--- packages/context.go
+++ packages/context.go
@@ -11,6 +11,19 @@ import (
 var wg sync.WaitGroup
 
 func Context(c *cli.Context) {
+	switch c.GlobalInt("m") {
+	case 1:
+		withTimeout()
+		break
+	case 2:
+		withValue()
+		break
+	case 3:
+		withCancel()
+	}
+}
+
+func withTimeout() {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	wg.Add(2)
 	go work(ctx, "work1")
@@ -19,7 +32,29 @@ func Context(c *cli.Context) {
 	wg.Wait()
 }
 
+func withValue() {
+	fmt.Println("with value")
+	ctxWithTimeout, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx := context.WithValue(ctxWithTimeout, "k", "bbb")
+	wg.Add(1)
+	go work(ctx, "work with value")
+	time.Sleep(2 * time.Second)
+	wg.Wait()
+}
+
+func withCancel() {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	wg.Add(1)
+	go work(ctx, "work with cancel")
+	go func() {
+		time.Sleep(2 * time.Second)
+		cancelFunc()
+	}()
+	wg.Wait()
+}
+
 func work(ctx context.Context, str string) {
+	fmt.Println("key:", ctx.Value("k"))
 	for {
 		select {
 		case <-ctx.Done():
